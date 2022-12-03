7bdd41cca8532526166d97b49b314099ab88c380
George <fed.georg @ yandex dot ru>
2022-12-04 02:05:36 +0300

    some refactoring

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,5 +5,17 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Ok"))
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version": version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server internal error", http.StatusInternalServerError)
+	}
 }
---
+++ cmd/api/helpers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type envelope map[string]interface{}
+
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	bytes, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	bytes = append(bytes, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(bytes)
+
+	return nil
+}
