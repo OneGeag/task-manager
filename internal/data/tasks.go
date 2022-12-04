c9ae8ae791294c252d2f18228faada8af33701ef
George <fed.georg @ yandex dot ru>
2022-12-04 20:10:07 +0300

    new data was added

--- internal/data/tasks.go
+++ internal/data/tasks.go
@@ -6,6 +6,7 @@ type Task struct {
 	ID int64 `json:"id"`
 	Title string `json:"title"`
 	Description string `json:"description"`
+	Status string `json:"status"`
 	CreatedAt time.Time `json:"-"`
 	Expires time.Time `json:"-"`
 }
